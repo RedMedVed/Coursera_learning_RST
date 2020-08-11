5d04c703068121614a693b59490b3710b14ff927
Sukharkov Maxim <suharkov.maxim@gmail.com>
2020-08-11 21:18:17 +0300

    First function works

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,3 +1,14 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-  
+  if(pollutant %in% c('sulfate', 'nitrate')) {
+    full_path <- paste(getwd(), '/', directory, '/', sep = '')
+    total = 0
+    len = 0
+    for(i in id) {
+      file = paste(full_path, sprintf('%03d', i), '.csv', sep = '')
+      f <- read.csv(file)
+      total <- total + sum(f[[pollutant]], na.rm = TRUE)
+      len <- len + length(f[[pollutant]][!is.na(f[[pollutant]])])
+    }
+  } else {print('No such pollutant, try again (sulfate or nitrate)')}
+  total / len
 }
