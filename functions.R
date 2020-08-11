b5957888b20c00c24b766d800c8b0a929981d08a
Sukharkov Maxim <suharkov.maxim@gmail.com>
2020-08-11 15:07:36 +0300

    Week 2 test passed

--- functions.R
+++ functions.R
@@ -1,5 +1,5 @@
 z <- function(x) {
-  y <- if(x>3) {
+  y <- if (x > 3) {
     10
   } else {
     2
@@ -7,4 +7,38 @@ z <- function(x) {
   return(y)
 }
 
+g <- function(l) {
+  while (l >= 8 & l <= 15) {
+    print(l)
+    coin <- rbinom(1, 1, 0.5)
+    if (coin == 1) {
+      l <- l + 1
+    } else {
+      l <- l - 1
+    }
+  }
+}
 
+adder <- function(d, f) {
+  return(d + f)
+}
+
+make.power <- function(n) {
+  pow <- function(x) {
+    x ^ n
+  }
+  pow
+}
+
+h <- function(x, y = NULL, d = 3L) {
+  z <- cbind(x, d)
+  if(!is.null(y))
+    z <- z + y
+  else
+    z <- z + f
+  g <- x + y / z
+  if(d == 3L)
+    return(g)
+  g <- g + 10
+  g
+}
