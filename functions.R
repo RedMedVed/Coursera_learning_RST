e2bf311a80b79f1772a4b037a23e1b5a67918637
Sukharkov Maxim <suharkov.maxim@gmail.com>
2020-08-14 16:43:50 +0300

    Some functions of week 4 added

--- functions.R
+++ functions.R
@@ -42,3 +42,30 @@ h <- function(x, y = NULL, d = 3L) {
   g <- g + 10
   g
 }
+
+distr1 <- function(seed1, amount = 100) {
+  set.seed(seed1)
+  x <- rnorm(amount)
+  e <- rnorm(amount, 0, 2)
+  y <- 0.5 + 2 * x + e
+  print(summary(y))
+  plot(x, y)
+}
+
+distr2 <- function(seed2, amount = 100) {
+  set.seed(seed2)
+  x <- rbinom(amount, 1, 0.5)
+  e <- rnorm(amount, 0, 2)
+  y <- 0.5 + 2 * x + e
+  print(summary(y))
+  plot(x, y)
+}
+
+distr3 <- function(seed3, amount = 100) {
+  set.seed(seed3)
+  x <- rnorm(amount)
+  log.mu <- 0.5 + 0.3 * x
+  y <- rpois(amount, exp(log.mu))
+  print(summary(y))
+  plot(x, y)
+}
