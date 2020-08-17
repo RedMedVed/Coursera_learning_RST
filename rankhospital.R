137016ee16ed3743c9a2ede17eda1ae67d0c9006
Sukharkov Maxim <suharkov.maxim@gmail.com>
2020-08-17 17:01:44 +0300

    ranking by outcome - task #2

--- rankhospital.R
+++ rankhospital.R
@@ -1,10 +1,11 @@
-best <- function(state, outcome) {
+rankhospital <- function(state, outcome, num = 'best') {
   ## Read outcome data
   hosp_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
   
   # in this particular case we do not need all the data, so let's take a subset
   data <- subset(hosps, select = c(2, 7, 11, 17, 23))
   colnames(data) <- c('hospital.name', 'State', 'heart attack', 'heart failure', 'pneumonia')
+  
   
   ## Check that state and outcome are valid
   if(!state %in% unique(data$State)) {
@@ -17,5 +18,19 @@ best <- function(state, outcome) {
   ## Return hospital name in that state with lowest 30-day death
   ## rate
   data <- data[data$State == state, ]
-  data[which.min(data[, outcome]), 1]
+
+  #can't find any ways to remove 'Not Available' from dataframe
+  #will use other method
+  #and choose the hospital with desired rank
+  if(num == 'best') {
+    data <- data[order(as.numeric(data[, outcome]), data$hospital.name),]
+    data[1, 1]
+  } else if(num == 'worst') {
+    data <- data[order(as.numeric(data[, outcome]), data$hospital.name, na.last = FALSE),]
+    tail(data, n = 1)[, 1]
+  } else {
+    data <- data[order(as.numeric(data[, outcome]), data$hospital.name),]
+    data[num, 1]
+  }
+  
 }
